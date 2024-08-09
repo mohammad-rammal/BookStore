a4f6f42ccfb184174536b492e9b9ad29610bf45c
mohammad-rammal <mohammad.rammal@hotmail.com>
2024-08-10 02:07:12 +0300

    remove try and catch

--- routes/authors.js
+++ routes/authors.js
@@ -1,5 +1,10 @@
 const express = require("express");
-const { Author, validateCreateAuthor, validateUpdateAuthor } = require("../models/Author");
+const {
+    Author,
+    validateCreateAuthor,
+    validateUpdateAuthor,
+} = require("../models/Author");
+const asyncHandler = require("express-async-handler");
 
 const router = express.Router();
 
@@ -9,15 +14,13 @@ const router = express.Router();
  *  @method  Get
  *  @access  public
  */
-router.get("/", async (req, res) => {
-    try {
+router.get(
+    "/",
+    asyncHandler(async (req, res) => {
         const authorList = await Author.find(); //.sort({ firstName: 1 }).select("firstName lastName -_id")//-1 inverse
         res.status(200).json(authorList);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Problem in server (Something Wrong) " });
-    }
-});
+    })
+);
 
 /**
  *  @desc    Get author by id
@@ -25,8 +28,9 @@ router.get("/", async (req, res) => {
  *  @method  Get
  *  @access  public
  */
-router.get("/:id", async (req, res) => {
-    try {
+router.get(
+    "/:id",
+    asyncHandler(async (req, res) => {
         const author = await Author.findById(req.params.id);
 
         if (author) {
@@ -34,11 +38,8 @@ router.get("/:id", async (req, res) => {
         } else {
             res.status(400).json({ message: "Author not found" });
         }
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Problem in server " });
-    }
-});
+    })
+);
 
 /**
  *  @desc    Create new author
@@ -46,17 +47,15 @@ router.get("/:id", async (req, res) => {
  *  @method  Post
  *  @access  public
  */
-router.post("/", async (req, res) => {
-    //console.log(req.body) // req from client (body) is json file not object,, so must convert json to object
-
-    const { error } = validateCreateAuthor(req.body);
-
-    if (error) {
-        return res.status(400).json({ message: error.details[0].message });
-    }
-
-    // bz promise will have success and error
-    try {
+router.post(
+    "/",
+    asyncHandler(async (req, res) => {
+        //console.log(req.body) // req from client (body) is json file not object,, so must convert json to object
+        const { error } = validateCreateAuthor(req.body);
+        if (error) {
+            return res.status(400).json({ message: error.details[0].message });
+        }
+        // bz promise will have success and error
         const author = new Author({
             firstName: req.body.firstName,
             lastName: req.body.lastName,
@@ -66,11 +65,8 @@ router.post("/", async (req, res) => {
 
         const result = await author.save(); // save will get promise , so must have await, so must be async
         res.status(201).json(author); // 201 created successfully
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Problem in server " });
-    }
-});
+    })
+);
 
 /**
  *  @desc    Update a author
@@ -78,12 +74,13 @@ router.post("/", async (req, res) => {
  *  @method  Put
  *  @access  public
  */
-router.put("/:id", async (req, res) => {
-    const { error } = validateUpdateAuthor(req.body);
-    if (error) {
-        return res.status(400).json({ message: error.details[0].message });
-    }
-    try {
+router.put(
+    "/:id",
+    asyncHandler(async (req, res) => {
+        const { error } = validateUpdateAuthor(req.body);
+        if (error) {
+            return res.status(400).json({ message: error.details[0].message });
+        }
         const author = await Author.findByIdAndUpdate(
             req.params.id,
             {
@@ -97,11 +94,8 @@ router.put("/:id", async (req, res) => {
             { new: true }
         );
         res.status(200).json(author);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Problem in server " });
-    }
-});
+    })
+);
 
 /**
  *  @desc    Delete a author
@@ -109,21 +103,17 @@ router.put("/:id", async (req, res) => {
  *  @method  Delete
  *  @access  public
  */
-router.delete("/:id", async (req, res) => {
-    try {
-        const author = await Author.findById(req.params.id)
+router.delete(
+    "/:id",
+    asyncHandler(async (req, res) => {
+        const author = await Author.findById(req.params.id);
         if (author) {
-            await Author.findByIdAndDelete(req.params.id)
+            await Author.findByIdAndDelete(req.params.id);
             res.status(200).json({ message: "Author has been deleted" });
         } else {
             res.status(404).json({ message: "Author not found" });
         }
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Problem in server " });
-    }
-});
-
-
+    })
+);
 
 module.exports = router;
