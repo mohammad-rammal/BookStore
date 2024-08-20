3a43b80d9df01216c7091fbedf85052f71eb0ff1
mohammad-rammal <mohammad.rammal@hotmail.com>
2024-08-20 16:33:17 +0300

    create login controller

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
-const { validateRegisterUser, User } = require("../models/User");
+const { validateRegisterUser, User, validateLoginUser } = require("../models/User");
 
 const router = express.Router();
 
@@ -38,6 +38,35 @@ router.post("/register", asyncHandler(async (req, res) => {
     const { password, ...other } = result._doc;
 
     res.status(201).json({ ...other, token });
+
+}));
+
+/**
+ *  @desc    Login User
+ *  @route   /api/auth/login
+ *  @method  POST
+ *  @access  public
+ */
+router.post("/login", asyncHandler(async (req, res) => {
+    const { error } = validateLoginUser(req.body);
+    if (error) {
+        return res.status(400).json({ message: error.details[0].message });
+    }
+
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        return res.status(400).json({ message: "The user not registered before or wrong password" })
+    }
+
+    const isPasswordMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isPasswordMatch) {
+        return res.status(400).json({ message: "The user not registered before or wrong password" })
+    }
+
+    const token = null;
+    const { password, ...other } = user._doc;
+
+    res.status(200).json({ ...other, token });
 
 }));
 
