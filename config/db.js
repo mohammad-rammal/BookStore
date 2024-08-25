3a4a26ecc58011f06b812efa44bc3ffa6639bd56
mohammad-rammal <mohammad.rammal@hotmail.com>
2024-08-25 03:17:31 +0300

    Finall

--- config/db.js
+++ config/db.js
@@ -4,8 +4,6 @@ async function connectToDB() {
     console.log("Attempting to connect to MongoDB...");
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
             serverSelectionTimeoutMS: 50000
         });
         console.log("Connected Successfully To Mongodb...");
